5557fbaace509bd028b840ba7a1f46a23c9fe60f
Pomin Wu <pomin5@gmail.com>
2021-02-18 10:00:05 +0800

    feat: add storage basics

--- lib/Rocks/Storage.pm
+++ lib/Rocks/Storage.pm
@@ -1,3 +1,32 @@
 package Rocks::Storage;
 
+use v5.26;
+use utf8;
+use warnings;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+use Mojo::Pg;
+
+my $schema = do { local $/ = undef; <DATA> };
+
+sub connect ($user, $pass, $host, $name)
+{
+    Mojo::Pg->new("postgresql://$user:$pass\@$host/$name")
+}
+
+sub migrate ($db)
+{
+    $db->migrations->from_string($schema)->migrate
+}
+
+sub rollback ($db)
+{
+    $db->migrations->migrate(0)
+}
+
 1;
+
+__DATA__
+-- 1 up
+-- 1 down
--- start.pl
+++ start.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl -w
 
 use v5.26;
+use Dotenv -load;
 use Mojolicious::Lite -signatures;
 use Moo;
 use Rocks::Models::Project;
 use Rocks::Models::Event;
+use Rocks::Storage;
 
 my $e = Rocks::Models::Event->new(name => "g0v 大松");
 my $p = Rocks::Models::Project->from_record({
@@ -17,3 +19,7 @@ my $p = Rocks::Models::Project->from_record({
 
 use Data::Dumper;
 say Dumper($p);
+
+my $db = Rocks::Storage::connect(@ENV{qw[DB_USER DB_PASS DB_HOST DB_NAME]});
+Rocks::Storage::migrate($db);
+Rocks::Storage::rollback($db);
